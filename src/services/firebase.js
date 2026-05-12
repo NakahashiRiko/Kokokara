846fb427736f15ba85841f3a9d098c51a1dd516d
NakahashiRiko <k25096kk@aitech.ac.jp>
2026-05-12 16:51:39 +0900

    htmlのコードに合わせた修正。

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 //import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
--- src/services/healthService.js
+++ src/services/healthService.js
@@ -1,4 +1,4 @@
-import { db, auth } from './firebase';
+import { db, auth } from './firebase.js';
 import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
 
 /**
